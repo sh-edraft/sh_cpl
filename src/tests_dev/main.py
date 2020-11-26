cd7c12bba43710049b0681d15323a0ad2023ab14
Sven Heidemann <sven.heidemann@sh-edraft.de>
2020-11-26 17:57:51 +0100

    Improved dev test

--- src/tests_dev/main.py
+++ src/tests_dev/main.py
@@ -1,25 +1,4 @@
-from sh_edraft.hosting import ApplicationHost
-from sh_edraft.hosting.base.application_base import ApplicationBase
-
-
-class Program(ApplicationBase):
-
-    def __init__(self):
-        ApplicationBase.__init__(self)
-
-        self._app_host = ApplicationHost('CPL_DEV_Test')
-        self._config = self._app_host.services.config
-        self._services = self._app_host.services
-
-    def create_configuration(self):
-        self._config.create()
-
-    def create_services(self):
-        self._services.create()
-
-    def main(self):
-        print('RUN')
-
+from tests_dev.program import Program
 
 if __name__ == '__main__':
     program = Program()
---
+++ src/tests_dev/program.py
@@ -0,0 +1,21 @@
+from sh_edraft.hosting import ApplicationHost
+from sh_edraft.hosting.base.application_base import ApplicationBase
+
+
+class Program(ApplicationBase):
+
+    def __init__(self):
+        ApplicationBase.__init__(self)
+
+        self._app_host = ApplicationHost('CPL_DEV_Test')
+        self._config = self._app_host.services.config
+        self._services = self._app_host.services
+
+    def create_configuration(self):
+        self._config.create()
+
+    def create_services(self):
+        self._services.create()
+
+    def main(self):
+        print('RUN')
