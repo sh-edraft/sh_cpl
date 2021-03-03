ed0b761502639b721a59e8f55da33076d37c8dc5
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-03 10:51:33 +0100

    Removed old sources

--- src_old/sh_edraft/logging/model/logging_settings_name.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class LoggingSettingsName(Enum):
-
-    path = 'Path'
-    filename = 'Filename'
-    console_level = 'ConsoleLogLevel'
-    file_level = 'FileLogLevel'
--- src_old/sh_edraft/mailing/model/email_client_settings_name.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class EMailClientSettingsName(Enum):
-
-    host = 'Host'
-    port = 'Port'
-    user_name = 'UserName'
-    credentials = 'Credentials'
