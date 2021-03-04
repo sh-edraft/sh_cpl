226123a81faef42fddee22fc984e21dda22199e1
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-04 07:15:48 +0100

    Improved application

--- src/cpl/application/application_abc.py
+++ src/cpl/application/application_abc.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Type, Optional
 
 from cpl.application.application_host_abc import ApplicationHostABC
+from cpl.application.application_runtime_abc import ApplicationRuntimeABC
 from cpl.application.startup_abc import StartupABC
 from cpl.configuration.configuration_abc import ConfigurationABC
 from cpl.console.console import Console
@@ -14,8 +15,9 @@ class ApplicationABC(ABC):
     def __init__(self):
         self._startup: Optional[StartupABC] = None
         self._app_host: Optional[ApplicationHostABC] = None
-        self._services: Optional[ServiceProviderABC] = None
         self._configuration: Optional[ConfigurationABC] = None
+        self._runtime: Optional[ApplicationRuntimeABC] = None
+        self._services: Optional[ServiceProviderABC] = None
 
     def use_startup(self, startup: Type[StartupABC]):
         self._startup = startup()
@@ -23,6 +25,7 @@ class ApplicationABC(ABC):
     def build(self):
         if self._startup is not None:
             self._app_host = self._startup.create_application_host()
+            self._runtime = self._app_host.application_runtime
             self._configuration = self._startup.create_configuration()
             self._services = self._startup.create_services()
 
