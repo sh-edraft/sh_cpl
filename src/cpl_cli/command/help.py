31515d2c3a1237cfe0f938b653681eab76267609
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-09 22:31:54 +0100

    Fixed help command & edited project structure doc

--- src/cpl_cli/command/help.py
+++ src/cpl_cli/command/help.py
@@ -10,14 +10,14 @@ class Help(CommandABC):
     def run(self, args: list[str]):
         Console.write_line('Available Commands:')
         commands = [
-            ['build (-b|-B)', 'Prepares files for publish into an output directory named dist/ at the given output path. Must be executed from within a workspace directory.'],
-            ['generate (-g|-G)', 'Generate a new file.'],
-            ['help (-h|-H)', 'Lists available command and their short descriptions.'],
-            ['new (-n|-N)', 'Creates new CPL project.'],
-            ['start (-s|-S)', 'Starts CPL project, restarting on file changes'],
-            ['publish (-p|-P)', 'Prepares files for publish into an output directory named dist/ at the given output path and executes setup.py. Must be executed from within a workspace directory.'],
-            ['update (-u|-u)', 'Update CPL and project dependencies.'],
-            ['version (-v|-V)', 'Outputs CPL CLI version.']
+            ['build (b|B)', 'Prepares files for publish into an output directory named dist/ at the given output path. Must be executed from within a workspace directory.'],
+            ['generate (g|G)', 'Generate a new file.'],
+            ['help (h|H)', 'Lists available command and their short descriptions.'],
+            ['new (n|N)', 'Creates new CPL project.'],
+            ['start (s|S)', 'Starts CPL project, restarting on file changes'],
+            ['publish (p|P)', 'Prepares files for publish into an output directory named dist/ at the given output path and executes setup.py. Must be executed from within a workspace directory.'],
+            ['update (u|u)', 'Update CPL and project dependencies.'],
+            ['version (v|V)', 'Outputs CPL CLI version.']
         ]
         for name, description in commands:
             Console.set_foreground_color('blue')
