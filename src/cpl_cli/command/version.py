973fd39f441335cfc16bc3bd357e7539da035ff0
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-10 20:55:33 +0100

    Improved colors

--- src/cpl_cli/command/version.py
+++ src/cpl_cli/command/version.py
@@ -19,23 +19,17 @@ class Version(CommandABC):
     def run(self, args: list[str]):
         Console.set_foreground_color(ForegroundColor.yellow)
         Console.banner('CPL CLI')
-        Console.set_foreground_color('default')
+        Console.set_foreground_color(ForegroundColor.default)
         if '__version__' in dir(cpl_cli):
-            Console.set_foreground_color(ForegroundColor.yellow)
-            Console.write_line(f'Common Python Library CLI: ')
-            Console.set_foreground_color(ForegroundColor.default)
+            Console.write_line(f'Common Python library CLI: ')
             Console.write(cpl_cli.__version__)
 
-        Console.set_foreground_color(ForegroundColor.blue)
         Console.write_line(f'Python: ')
-        Console.set_foreground_color(ForegroundColor.default)
         Console.write(f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}')
 
         Console.write_line(f'OS: {platform.system()} {platform.processor()}')
 
-        Console.set_foreground_color(ForegroundColor.yellow)
         Console.write_line('\nCPL packages:')
-        Console.set_foreground_color(ForegroundColor.default)
         packages = []
         for importer, modname, is_pkg in pkgutil.iter_modules(cpl.__path__):
             module = importer.find_module(modname).load_module(modname)
@@ -44,9 +38,7 @@ class Version(CommandABC):
 
         Console.table(['Name', 'Version'], packages)
 
-        Console.set_foreground_color(ForegroundColor.blue)
         Console.write_line('\nPython packages:')
-        Console.set_foreground_color(ForegroundColor.default)
         packages = []
         dependencies = dict(tuple(str(ws).split()) for ws in pkg_resources.working_set)
         for p in dependencies:
