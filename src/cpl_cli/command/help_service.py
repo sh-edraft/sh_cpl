a9f3f75a793a4f21e241951fb58b236bcaa43e0a
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-14 15:15:07 +0100

    Improved help command

--- src/cpl_cli/command/help_service.py
+++ src/cpl_cli/command/help_service.py
@@ -14,9 +14,11 @@ class HelpService(CommandABC):
             ['build (b|B)', 'Prepares files for publish into an output directory named dist/ at the given output path. Must be executed from within a workspace directory.'],
             ['generate (g|G)', 'Generate a new file.'],
             ['help (h|H)', 'Lists available command and their short descriptions.'],
+            ['install (i|I)', 'With argument installs packages to project, without argument installs project dependencies.'],
             ['new (n|N)', 'Creates new CPL project.'],
             ['start (s|S)', 'Starts CPL project, restarting on file changes'],
             ['publish (p|P)', 'Prepares files for publish into an output directory named dist/ at the given output path and executes setup_template.py. Must be executed from within a workspace directory.'],
+            ['uninstall (ui|UI)', 'Uninstalls packages from project.'],
             ['update (u|u)', 'Update CPL and project dependencies.'],
             ['version (v|V)', 'Outputs CPL CLI version.']
         ]
