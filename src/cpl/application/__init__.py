d61da17ebd54b956008f9b0b90aee45a98dd2419
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-15 18:37:21 +0100

    Publish 2021.4.1.post5

--- src/cpl/application/__init__.py
+++ src/cpl/application/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl.application'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.1.post4'
+__version__ = '2021.4.1.post5'
 
 from collections import namedtuple
 
@@ -28,4 +28,4 @@ from .application_runtime_abc import ApplicationRuntimeABC
 from .startup_abc import StartupABC
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='04', micro='01-4')
+version_info = VersionInfo(major='2021', minor='04', micro='01-5')
--- src/cpl/logging/__init__.py
+++ src/cpl/logging/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl.logging'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.1.post4'
+__version__ = '2021.4.1.post5'
 
 from collections import namedtuple
 
@@ -27,4 +27,4 @@ from .logging_settings import LoggingSettings
 from .logging_settings_name_enum import LoggingSettingsNameEnum
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='04', micro='01-4')
+version_info = VersionInfo(major='2021', minor='04', micro='01-5')
