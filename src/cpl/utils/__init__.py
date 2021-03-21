99091a2540fbebe4646023e48235fcb0b39a3080
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-21 20:22:04 +0100

    Publish for 2021.4.2

--- src/cpl/utils/__init__.py
+++ src/cpl/utils/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl.utils'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.1.post15'
+__version__ = '2021.4.2'
 
 from collections import namedtuple
 
@@ -25,4 +25,4 @@ from .string import String
 from .pip import Pip
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='04', micro='01-15')
+version_info = VersionInfo(major='2021', minor='04', micro='02')
