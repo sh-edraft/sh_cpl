cf2f29329011a2e1756126f63c275adaf496628d
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-30 11:03:16 +0200

    Publish 2021.4.0rc2

--- src/cpl/time/__init__.py
+++ src/cpl/time/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl.time'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.0rc1'
+__version__ = '2021.4.0rc2'
 
 from collections import namedtuple
 
@@ -24,4 +24,4 @@ from .time_format_settings import TimeFormatSettings
 from .time_format_settings_names_enum import TimeFormatSettingsNamesEnum
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='04', micro='0rc1')
+version_info = VersionInfo(major='2021', minor='04', micro='0rc2')
