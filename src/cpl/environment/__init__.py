6cdd3376f133960deda5b337b662811b0035a9a1
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-30 21:01:10 +0200

    Publish 2021.4.0rc3

--- src/cpl/environment/__init__.py
+++ src/cpl/environment/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl.environment'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.0rc2'
+__version__ = '2021.4.0rc3'
 
 from collections import namedtuple
 
@@ -25,4 +25,4 @@ from .environment_name_enum import EnvironmentNameEnum
 from .application_environment import ApplicationEnvironment
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='04', micro='0rc2')
+version_info = VersionInfo(major='2021', minor='04', micro='0rc3')
