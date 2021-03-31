18b2cd8f2fefc5bb71b8293d1a66bb0194912166
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-03-31 11:01:08 +0200

    Publish 2021.4.0rc4

--- src/cpl/database/context/__init__.py
+++ src/cpl/database/context/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl.database.context'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.0rc3'
+__version__ = '2021.4.0rc4'
 
 from collections import namedtuple
 
@@ -24,4 +24,4 @@ from .database_context import DatabaseContext
 from .database_context_abc import DatabaseContextABC
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='04', micro='0rc3')
+version_info = VersionInfo(major='2021', minor='04', micro='0rc4')
