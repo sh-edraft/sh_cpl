3ba5c10fadef2d59980c752d951c50b6cfdf7bec
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-04-07 19:19:20 +0200

    Publish CLI 2021.4

--- src/cpl_cli/command/__init__.py
+++ src/cpl_cli/command/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-sh_cpl sh-edraft Common Python library
+sh_cpl-cli sh-edraft Common Python library CLI
 ~~~~~~~~~~~~~~~~~~~
 
-sh-edraft Common Python library
+sh-edraft Common Python library Command Line Interface
 
 :copyright: (c) 2020 - 2021 sh-edraft.de
 :license: MIT, see LICENSE for more details.
@@ -15,7 +15,7 @@ __title__ = 'cpl_cli.command'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.post1'
+__version__ = '2021.4'
 
 from collections import namedtuple
 
@@ -28,4 +28,4 @@ from .publish_service import PublishService
 from .version_service import VersionService
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='4', micro='post1')
+version_info = VersionInfo(major='2021', minor='4', micro='None')
--- src/cpl_cli/templates/new/library/__init__.py
+++ src/cpl_cli/templates/new/library/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-sh_cpl sh-edraft Common Python library
+sh_cpl-cli sh-edraft Common Python library CLI
 ~~~~~~~~~~~~~~~~~~~
 
-sh-edraft Common Python library
+sh-edraft Common Python library Command Line Interface
 
 :copyright: (c) 2020 - 2021 sh-edraft.de
 :license: MIT, see LICENSE for more details.
@@ -15,11 +15,11 @@ __title__ = 'cpl_cli.templates.new.library'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.post1'
+__version__ = '2021.4'
 
 from collections import namedtuple
 
 # imports:
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='4', micro='post1')
+version_info = VersionInfo(major='2021', minor='4', micro='None')
