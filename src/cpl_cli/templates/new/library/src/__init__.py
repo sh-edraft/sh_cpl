76a6ba11fbc79582cd626355717cfeab56b45b50
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-04-07 17:01:39 +0200

    Publish 2021.4.post1 for local dev

--- src/cpl_cli/templates/new/library/src/__init__.py
+++ src/cpl_cli/templates/new/library/src/__init__.py
@@ -15,11 +15,11 @@ __title__ = 'cpl_cli.templates.new.library.src'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.0'
+__version__ = '2021.4.post1'
 
 from collections import namedtuple
 
 # imports:
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='04', micro='0')
+version_info = VersionInfo(major='2021', minor='4', micro='post1')
