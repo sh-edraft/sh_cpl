0f836b5c7d3cc9b40c37075a6bf10d796d93df5b
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-04-08 20:28:34 +0200

    Published after file move

--- src/cpl_cli/templates/new/console/src/tests/__init__.py
+++ src/cpl_cli/templates/new/console/src/tests/__init__.py
@@ -11,7 +11,7 @@ sh-edraft Common Python library Command Line Interface
 
 """
 
-__title__ = 'cpl_cli.templates.new.console.src.tests'
+__title__ = 'cpl_cli.templates.new.console.tests'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
--- src/cpl_cli/templates/new/library/src/__init__.py
+++ src/cpl_cli/templates/new/library/src/__init__.py
@@ -11,7 +11,7 @@ sh-edraft Common Python library Command Line Interface
 
 """
 
-__title__ = 'cpl_cli.templates.new.library.src'
+__title__ = 'cpl_cli.templates.new.library'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
