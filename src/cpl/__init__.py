128fda60b031b1c41d36a5d06743da570e8fecd3
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-05-19 08:19:21 +0200

    Release 2021.4.1.rc2

--- src/cpl/__init__.py
+++ src/cpl/__init__.py
@@ -15,11 +15,11 @@ __title__ = 'cpl'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.post1.rc1'
+__version__ = '2021.4.1rc2'
 
 from collections import namedtuple
 
 # imports:
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='4', micro='post1.rc1')
+version_info = VersionInfo(major='2021', minor='4', micro='1.rc2')
--- src/cpl/dependency_injection/__init__.py
+++ src/cpl/dependency_injection/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl.dependency_injection'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.post1.rc1'
+__version__ = '2021.4.1rc2'
 
 from collections import namedtuple
 
@@ -28,4 +28,4 @@ from .service_provider import ServiceProvider
 from .service_provider_abc import ServiceProviderABC
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='4', micro='post1.rc1')
+version_info = VersionInfo(major='2021', minor='4', micro='1.rc2')
