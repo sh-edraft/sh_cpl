59f6843ea9e546fd21ff38a36a0ff006cde77f31
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-07-24 17:25:06 +0200

    Changed version

--- src/cpl_cli/command/__init__.py
+++ src/cpl_cli/command/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_cli.command'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.4.0.post3'
+__version__ = '2021.10.2'
 
 from collections import namedtuple
 
@@ -28,4 +28,4 @@ from .publish_service import PublishService
 from .version_service import VersionService
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='4', micro='0.post3')
+version_info = VersionInfo(major='2021', minor='10', micro='2')
