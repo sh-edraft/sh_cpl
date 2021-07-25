5c80cf222235efdc1fafa9f56a35e854da8f7f7b
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-07-25 12:24:58 +0200

    Added CPL Query

---
+++ src/cpl_query/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+cpl_query 
+~~~~~~~~~~~~~~~~~~~
+
+
+
+:copyright: (c)  
+:license: 
+
+"""
+
+__title__ = 'cpl_query'
+__author__ = ''
+__license__ = ''
+__copyright__ = 'Copyright (c)  '
+__version__ = '0.0.0'
+
+from collections import namedtuple
+
+# imports: 
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro')
+version_info = VersionInfo(major='0', minor='0', micro='0')
---
+++ src/cpl_query/main.py
@@ -0,0 +1,9 @@
+from cpl.console import Console
+
+
+def main():
+    Console.write_line('Hello World')
+
+
+if __name__ == '__main__':
+    main()
