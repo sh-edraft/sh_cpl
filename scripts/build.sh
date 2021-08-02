6867eb0e65cd9bf5d3e9c786ee3c097f278e7cde
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-08-02 14:14:50 +0200

    Improved scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,14 @@
 # activate venv
 source /home/sven/Nextcloud_Sven/Schreibtisch/git_sh-edraft_de/sh_cpl/cpl-env/bin/activate
 
-# CLI
-cd /home/sven/Nextcloud_Sven/Schreibtisch/git_sh-edraft_de/sh_cpl/
-cpl build
-
 # CPL
 cd /home/sven/Nextcloud_Sven/Schreibtisch/git_sh-edraft_de/sh_cpl/src/cpl
+cpl build
+
+# CLI
+cd /home/sven/Nextcloud_Sven/Schreibtisch/git_sh-edraft_de/sh_cpl/src/cpl_cli
+cpl build
+
+# CPL Query
+cd /home/sven/Nextcloud_Sven/Schreibtisch/git_sh-edraft_de/sh_cpl/src/cpl_query
 cpl build
