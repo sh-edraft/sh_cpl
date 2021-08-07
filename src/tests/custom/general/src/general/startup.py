2809a9cd821a3962fce7ffcc570703105bba0f65
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-08-07 11:24:17 +0200

    Improved imports

--- src/tests/custom/general/src/general/startup.py
+++ src/tests/custom/general/src/general/startup.py
@@ -2,7 +2,7 @@ from cpl_core.application.startup_abc import StartupABC
 from cpl_core.configuration.configuration_abc import ConfigurationABC
 from cpl_core.dependency_injection.service_collection_abc import ServiceCollectionABC
 from cpl_core.dependency_injection.service_provider_abc import ServiceProviderABC
-from cpl_core.environment import ApplicationEnvironmentABC
+from cpl_core.environment.application_environment_abc import ApplicationEnvironmentABC
 from cpl_core.logging.logger_service import Logger
 from cpl_core.logging.logger_abc import LoggerABC
 from cpl_core.mailing.email_client_service import EMailClient
