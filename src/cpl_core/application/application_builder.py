3903f853683b3f35425e28ee8dce830f4122c6f2
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-10-02 10:57:54 +0200

    Added logic to handle extensions before running the app

--- src/cpl_core/application/application_builder.py
+++ src/cpl_core/application/application_builder.py
@@ -1,9 +1,12 @@
-from typing import Type, Optional
+import types
+from typing import Type, Optional, Callable
 
 from cpl_core.application.application_abc import ApplicationABC
 from cpl_core.application.application_builder_abc import ApplicationBuilderABC
+from cpl_core.application.application_extension_abc import ApplicationExtensionABC
 from cpl_core.application.startup_abc import StartupABC
 from cpl_core.configuration.configuration import Configuration
+from cpl_core.console import Console
 from cpl_core.dependency_injection.service_collection import ServiceCollection
 
 
@@ -25,12 +28,25 @@ class ApplicationBuilder(ApplicationBuilderABC):
         self._environment = self._configuration.environment
         self._services = ServiceCollection(self._configuration)
 
+        self._extensions: list[Callable] = []
+
     def use_startup(self, startup: Type[StartupABC]):
         self._startup = startup()
+
+    def use_extension(self, extension: Type[ApplicationExtensionABC]):
+        if extension not in self._extensions:
+            self._extensions.append(extension)
 
     def build(self) -> ApplicationABC:
         if self._startup is not None:
             self._startup.configure_configuration(self._configuration, self._environment)
             self._startup.configure_services(self._services, self._environment)
 
-        return self._app(self._configuration, self._services.build_service_provider())
+        config = self._configuration
+        services = self._services.build_service_provider()
+
+        for ex in self._extensions:
+            extension = ex()
+            extension.run(config, services)
+
+        return self._app(config, services)
