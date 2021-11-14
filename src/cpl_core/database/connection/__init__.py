c3b48bfa41480b9990b799f7c2710e572702f999
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-11-14 09:55:27 +0100

    Changed version

--- src/cpl_core/database/connection/__init__.py
+++ src/cpl_core/database/connection/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_core.database.connection'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.10.1'
+__version__ = '2021.10.2'
 
 from collections import namedtuple
 
@@ -24,4 +24,4 @@ from .database_connection import DatabaseConnection
 from .database_connection_abc import DatabaseConnectionABC
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='10', micro='1')
+version_info = VersionInfo(major='2021', minor='10', micro='2')
