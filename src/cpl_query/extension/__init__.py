6518ae75164bbc2d9de2cad0b22ede50be163370
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-11-30 11:21:19 +0100

    Changed version of query

--- src/cpl_query/extension/__init__.py
+++ src/cpl_query/extension/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_query.extension'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.10.3'
+__version__ = '2021.11.0'
 
 from collections import namedtuple
 
@@ -27,4 +27,4 @@ from .ordered_iterable_abc import OrderedIterableABC
 from .ordered_iterable import OrderedIterable
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='10', micro='3')
+version_info = VersionInfo(major='2021', minor='11', micro='0')
