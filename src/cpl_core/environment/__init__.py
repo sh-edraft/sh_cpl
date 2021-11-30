95b5f99f1fe594568d88c30b62141dd6f9ea8f29
Sven Heidemann <sven.heidemann@sh-edraft.de>
2021-11-30 11:20:15 +0100

    Changed core version

--- src/cpl_core/environment/__init__.py
+++ src/cpl_core/environment/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_core.environment'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2021 sh-edraft.de'
-__version__ = '2021.10.2'
+__version__ = '2021.11.0'
 
 from collections import namedtuple
 
@@ -25,4 +25,4 @@ from .environment_name_enum import EnvironmentNameEnum
 from .application_environment import ApplicationEnvironment
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2021', minor='10', micro='2')
+version_info = VersionInfo(major='2021', minor='11', micro='0')
