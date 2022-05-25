d8168f024c385c8024638854b094c9c7ca9e5a3e
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-05-25 19:53:05 +0200

    Added logic to write PipPath & build dev2

--- tools/set_pip_urls/startup.py
+++ tools/set_pip_urls/startup.py
@@ -10,6 +10,8 @@ class Startup(StartupABC):
         StartupABC.__init__(self)
 
     def configure_configuration(self, configuration: ConfigurationABC, environment: ApplicationEnvironment) -> ConfigurationABC:
+        configuration.add_environment_variables('CPL_')
+        configuration.add_json_file('appsettings.json', optional=False, output=False)
         return configuration
 
     def configure_services(self, services: ServiceCollectionABC, environment: ApplicationEnvironment) -> ServiceProviderABC:
