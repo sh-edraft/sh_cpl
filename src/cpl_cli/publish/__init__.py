03ac00c6e3bed13a5a7fa55d2f6641bbecded8e6
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-05-25 16:51:52 +0200

    Bugfixes for set-version & CustomScriptService

--- src/cpl_cli/publish/__init__.py
+++ src/cpl_cli/publish/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_cli.publish'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2022 sh-edraft.de'
-__version__ = '2022.6.15.dev1'
+__version__ = '2022.6.15'
 
 from collections import namedtuple
 
@@ -24,4 +24,4 @@ from .publisher_abc import PublisherABC
 from .publisher_service import PublisherService
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='6', micro='15.dev1')
+version_info = VersionInfo(major='2022', minor='6', micro='15')
--- src/cpl_core/environment/__init__.py
+++ src/cpl_core/environment/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_core.environment'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2022 sh-edraft.de'
-__version__ = '2022.6.15.dev1'
+__version__ = '2022.6.15'
 
 from collections import namedtuple
 
@@ -25,4 +25,4 @@ from .environment_name_enum import EnvironmentNameEnum
 from .application_environment import ApplicationEnvironment
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='6', micro='15.dev1')
+version_info = VersionInfo(major='2022', minor='6', micro='15')
