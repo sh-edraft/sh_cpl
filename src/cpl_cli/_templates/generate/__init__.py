698f80adfe271ce948bdd32c6f75960b22bc3c4b
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-05-26 21:42:28 +0200

    Build dev6

--- src/cpl_cli/_templates/generate/__init__.py
+++ src/cpl_cli/_templates/generate/__init__.py
@@ -15,11 +15,11 @@ __title__ = 'cpl_cli._templates.generate'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2022 sh-edraft.de'
-__version__ = '2022.6.17.dev5'
+__version__ = '2022.6.17.dev6'
 
 from collections import namedtuple
 
 # imports:
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='6', micro='17.dev5')
+version_info = VersionInfo(major='2022', minor='6', micro='17.dev6')
