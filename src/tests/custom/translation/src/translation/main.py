39b6ca790caafb2b0d69cf3ccdad0699aec475f8
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-06-29 19:05:12 +0200

    Added test project

---
+++ src/tests/custom/translation/src/translation/main.py
@@ -0,0 +1,14 @@
+from cpl_core.application import ApplicationBuilder
+
+from translation.application import Application
+from translation.startup import Startup
+
+
+def main():
+    app_builder = ApplicationBuilder(Application)
+    app_builder.use_startup(Startup)
+    app_builder.build().run()
+
+
+if __name__ == '__main__':
+    main()
