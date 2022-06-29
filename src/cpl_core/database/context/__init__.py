61d3974a47b2e6b423f7d9847e145eb719530630
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-06-29 17:46:01 +0200

    Build Release 2022.6.0

--- src/cpl_core/database/context/__init__.py
+++ src/cpl_core/database/context/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_core.database.context'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2022 sh-edraft.de'
-__version__ = '2022.6.0rc2'
+__version__ = '2022.6.0'
 
 from collections import namedtuple
 
@@ -24,4 +24,4 @@ from .database_context import DatabaseContext
 from .database_context_abc import DatabaseContextABC
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='6', micro='0.rc2')
+version_info = VersionInfo(major='2022', minor='6', micro='0')
