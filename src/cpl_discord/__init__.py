5165f31066b08543607bd5c77bfe2e5946577d53
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-07-16 15:53:02 +0200

    Build cpl-discord

--- src/cpl_discord/__init__.py
+++ src/cpl_discord/__init__.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+cpl-discord sh-edraft Common Python library Discord
+~~~~~~~~~~~~~~~~~~~
+
+sh-edraft Common Python library link between discord.py and CPL
+
+:copyright: (c) 2021 - 2022 sh-edraft.de
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'cpl_discord'
+__author__ = 'Sven Heidemann'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2021 - 2022 sh-edraft.de'
+__version__ = '2022.7.0'
+
+from collections import namedtuple
+
+
 # imports
 # build-ignore
 
@@ -31,3 +53,6 @@ def get_discord_collection(services: 'ServiceCollectionABC') -> 'DiscordCollecti
     services.add_singleton(DiscordCollectionABC, collection)
     return collection
 # build-ignore-end
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro')
+version_info = VersionInfo(major='2022', minor='7', micro='0')
---
+++ src/cpl_discord/events/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+cpl-discord sh-edraft Common Python library Discord
+~~~~~~~~~~~~~~~~~~~
+
+sh-edraft Common Python library link between discord.py and CPL
+
+:copyright: (c) 2021 - 2022 sh-edraft.de
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'cpl_discord.events'
+__author__ = 'Sven Heidemann'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2021 - 2022 sh-edraft.de'
+__version__ = '2022.7.0'
+
+from collections import namedtuple
+
+
+# imports:
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro')
+version_info = VersionInfo(major='2022', minor='7', micro='0')
