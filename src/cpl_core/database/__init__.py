72ee59b2b425fc6f7bc6076a25b611fa64d0648a
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-07-17 15:52:53 +0200

    Deployed cpl-core==2022.7.0.post3

--- src/cpl_core/database/__init__.py
+++ src/cpl_core/database/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_core.database'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2022 sh-edraft.de'
-__version__ = '2022.7.0.post2'
+__version__ = '2022.7.0.post3'
 
 from collections import namedtuple
 
@@ -26,4 +26,4 @@ from .database_settings import DatabaseSettings
 from .table_abc import TableABC
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='7', micro='0.post2')
+version_info = VersionInfo(major='2022', minor='7', micro='0.post3')
