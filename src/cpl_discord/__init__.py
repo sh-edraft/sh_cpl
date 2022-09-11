452b30c899c2b0b981cb1ada6eb37c6ec0ba5262
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-09-11 19:30:19 +0200

    Build version

--- src/cpl_discord/__init__.py
+++ src/cpl_discord/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_discord'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2021 - 2022 sh-edraft.de'
-__version__ = '2022.7.0.post4'
+__version__ = '2022.10.1'
 
 from collections import namedtuple
 
@@ -55,4 +55,4 @@ def get_discord_collection(services: 'ServiceCollectionABC') -> 'DiscordCollecti
 # build-ignore-end
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='7', micro='0.post4')
+version_info = VersionInfo(major='2022', minor='10', micro='1')
