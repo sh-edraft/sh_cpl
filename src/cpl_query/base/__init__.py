e868a120f06d412eb6c7515c1b76a6528871fa27
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-09-13 19:38:16 +0200

    Build cpl-query

---
+++ src/cpl_query/base/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+cpl-query sh-edraft Common Python library Query
+~~~~~~~~~~~~~~~~~~~
+
+sh-edraft Common Python library Python integrated Queries
+
+:copyright: (c) 2021 - 2022 sh-edraft.de
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'cpl_query.base'
+__author__ = 'Sven Heidemann'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2021 - 2022 sh-edraft.de'
+__version__ = '2022.10.9'
+
+from collections import namedtuple
+
+
+# imports:
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro')
+version_info = VersionInfo(major='2022', minor='10', micro='9')
