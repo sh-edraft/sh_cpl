7f621b282ff6ff247d48d1ea02a04528cfef2225
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-11-18 08:37:16 +0100

    Build cpl-core@2022.7.0.post8

--- src/cpl_core/configuration/__init__.py
+++ src/cpl_core/configuration/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_core.configuration'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2022 sh-edraft.de'
-__version__ = '2022.10.0.post7'
+__version__ = '2022.10.0.post8'
 
 from collections import namedtuple
 
@@ -35,4 +35,4 @@ from .validator_abc import ValidatorABC
 from .variable_argument import VariableArgument
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='10', micro='0.post7')
+version_info = VersionInfo(major='2022', minor='10', micro='0.post8')
