6bf7c3a2bcb03d2588ff9146d0bd64ad108d17c8
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-12-01 12:19:49 +0100

    Reverted #125 fix

--- src/cpl_cli/configuration/__init__.py
+++ src/cpl_cli/configuration/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_cli.configuration'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2022 sh-edraft.de'
-__version__ = '2022.10.1'
+__version__ = '2022.10.1.post1'
 
 from collections import namedtuple
 
@@ -31,4 +31,4 @@ from .workspace_settings import WorkspaceSettings
 from .workspace_settings_name_enum import WorkspaceSettingsNameEnum
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='10', micro='1')
+version_info = VersionInfo(major='2022', minor='10', micro='1.post1')
