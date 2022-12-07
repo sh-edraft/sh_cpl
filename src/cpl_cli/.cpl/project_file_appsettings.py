6818fe9a5c239c4c964c11bee5579bbf761c175f
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-12-07 20:06:54 +0100

    Improved cpl new appsettings template

--- src/cpl_cli/.cpl/project_file_appsettings.py
+++ src/cpl_cli/.cpl/project_file_appsettings.py
@@ -1,3 +1,5 @@
+import textwrap
+
 from cpl_cli.abc.file_template_abc import FileTemplateABC
 
 
@@ -8,4 +10,20 @@ class ProjectFileAppsettings(FileTemplateABC):
         self._name = 'appsettings.json'
 
     def get_code(self) -> str:
-        return self._code
+        return textwrap.dedent("""\
+        {
+          "TimeFormatSettings": {
+            "DateFormat": "%Y-%m-%d",
+            "TimeFormat": "%H:%M:%S",
+            "DateTimeFormat": "%Y-%m-%d %H:%M:%S.%f",
+            "DateTimeLogFormat": "%Y-%m-%d_%H-%M-%S"
+          },
+        
+          "LoggingSettings": {
+            "Path": "logs/",
+            "Filename": "log_$start_time.log",
+            "ConsoleLogLevel": "ERROR",
+            "FileLogLevel": "WARN"
+          }
+        }
+        """)
