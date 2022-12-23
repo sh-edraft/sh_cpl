59263ece6e58defda76eeb971c84de8085f19754
Sven Heidemann <sven.heidemann@sh-edraft.de>
2022-12-23 17:28:38 +0100

    Added multiple instance handling #152

--- tests/custom/di/src/di/application.py
+++ tests/custom/di/src/di/application.py
@@ -4,8 +4,10 @@ from cpl_core.console.console import Console
 from cpl_core.dependency_injection import ServiceProviderABC
 from cpl_core.dependency_injection.scope import Scope
 from di.static_test import StaticTest
-from di.test_service_service import TestService
+from di.test_abc import TestABC
+from di.test_service import TestService
 from di.di_tester_service import DITesterService
+from di.tester import Tester
 
 
 class Application(ApplicationABC):
@@ -28,10 +30,6 @@ class Application(ApplicationABC):
             dit: DITesterService = scope.service_provider.get_service(DITesterService)
             dit.run()
 
-        # Console.write_line('Disposed:')
-        # ts1: TestService = scope1.service_provider.get_service(TestService)
-        # ts1.run()
-
         with self._services.create_scope() as scope:
             Console.write_line('Scope2')
             ts: TestService = scope.service_provider.get_service(TestService)
@@ -42,5 +40,6 @@ class Application(ApplicationABC):
         Console.write_line('Global')
         self._part_of_scoped()
         StaticTest.test()
-        with self._services.create_scope() as scope:
-            StaticTest.test()
+
+        self._services.get_service(Tester)
+        Console.write_line(self._services.get_services(list[TestABC]))
--- tests/custom/di/src/di/startup.py
+++ tests/custom/di/src/di/startup.py
@@ -2,8 +2,12 @@ from cpl_core.application import StartupABC
 from cpl_core.configuration import ConfigurationABC
 from cpl_core.dependency_injection import ServiceProviderABC, ServiceCollectionABC
 from cpl_core.environment import ApplicationEnvironment
-from di.test_service_service import TestService
+from di.test1_service import Test1Service
+from di.test2_service import Test2Service
+from di.test_abc import TestABC
+from di.test_service import TestService
 from di.di_tester_service import DITesterService
+from di.tester import Tester
 
 
 class Startup(StartupABC):
@@ -17,5 +21,9 @@ class Startup(StartupABC):
     def configure_services(self, services: ServiceCollectionABC, environment: ApplicationEnvironment) -> ServiceProviderABC:
         services.add_scoped(TestService)
         services.add_scoped(DITesterService)
+
+        services.add_singleton(TestABC, Test1Service)
+        services.add_singleton(TestABC, Test2Service)
+        services.add_singleton(Tester)
         
         return services.build_service_provider()
