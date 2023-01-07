e0ca7c2ae67d8b84068581c8c4d287bb54dc66dc
Sven Heidemann <sven.heidemann@sh-edraft.de>
2023-01-07 15:03:21 +0100

    Build fixed version

--- src/cpl_cli/validators/__init__.py
+++ src/cpl_cli/validators/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_cli.validators'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2023 sh-edraft.de'
-__version__ = '2022.12.1'
+__version__ = '2022.12.1.post1'
 
 from collections import namedtuple
 
@@ -23,4 +23,4 @@ from collections import namedtuple
 # imports:
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='12', micro='1')
+version_info = VersionInfo(major='2022', minor='12', micro='1.post1')
