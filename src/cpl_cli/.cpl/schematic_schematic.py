2c3664c1b961ab165c40097a812c4a034ab7eee6
Sven Heidemann <sven.heidemann@sh-edraft.de>
2023-01-15 10:01:05 +0100

    Fixed schematic schematic

--- src/cpl_cli/.cpl/schematic_schematic.py
+++ src/cpl_cli/.cpl/schematic_schematic.py
@@ -40,7 +40,7 @@ class Schematic(GenerateSchematicABC):
                     []
                 )
         """
-        return self.build_code_str(code, Name=self._class_name)
+        return self.build_code_str(code, Name=self._class_name, NameLower=self._class_name.lower())
 
     @classmethod
     def register(cls):
--- src/cpl_cli/command/__init__.py
+++ src/cpl_cli/command/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_cli.command'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2020 - 2023 sh-edraft.de'
-__version__ = '2022.12.1.post2'
+__version__ = '2022.12.1.post3'
 
 from collections import namedtuple
 
@@ -29,4 +29,4 @@ from .publish_service import PublishService
 from .version_service import VersionService
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='12', micro='1.post2')
+version_info = VersionInfo(major='2022', minor='12', micro='1.post3')
