43710717c9e5c0bb6f98e2a6c6d9155dacc01a00
Sven Heidemann <sven.heidemann@sh-edraft.de>
2023-03-11 01:33:56 +0100

    Fixed cpl-discord deps

--- src/cpl_discord/configuration/__init__.py
+++ src/cpl_discord/configuration/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'cpl_discord.configuration'
 __author__ = 'Sven Heidemann'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2022 - 2023 sh-edraft.de'
-__version__ = '2022.12.2'
+__version__ = '2022.12.2.post1'
 
 from collections import namedtuple
 
@@ -24,4 +24,4 @@ from collections import namedtuple
 from .discord_bot_settings import DiscordBotSettings
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro')
-version_info = VersionInfo(major='2022', minor='12', micro='2')
+version_info = VersionInfo(major='2022', minor='12', micro='2.post1')
