04f610c799578e60f75ab19ba0e27d3134f513e7
Sven Heidemann <sven.heidemann@sh-edraft.de>
2023-04-07 14:41:11 +0200

    Removed obsolete pipes

--- src/cpl_core/pipes/__init__.py
+++ src/cpl_core/pipes/__init__.py
@@ -19,15 +19,10 @@ __version__ = "2023.4.0"
 
 from collections import namedtuple
 
-
 # imports:
 from .bool_pipe import BoolPipe
-from .first_char_to_lower_pipe import FirstCharToLowerPipe
-from .first_to_upper_pipe import FirstToUpperPipe
 from .ip_address_pipe import IPAddressPipe
 from .pipe_abc import PipeABC
-from .to_camel_case_pipe import ToCamelCasePipe
-from .to_snake_case_pipe import ToSnakeCasePipe
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro")
 version_info = VersionInfo(major="2023", minor="4", micro="0")
