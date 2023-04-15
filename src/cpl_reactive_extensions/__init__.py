3ee617ee38cf5e6ad7fb70e9add723feb9c79f11
Sven Heidemann <sven.heidemann@sh-edraft.de>
2023-04-15 21:49:34 +0200

    Build project

--- src/cpl_reactive_extensions/__init__.py
+++ src/cpl_reactive_extensions/__init__.py
@@ -1,1 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+cpl-reactive-extensions CPL Simple ReactiveX implementation
+~~~~~~~~~~~~~~~~~~~
+
+CPL Simple ReactiveX implementation, see RxJS and RxPy for detailed implementation.
+
+:copyright: (c) 2023 sh-edraft.de
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = "cpl_reactive_extensions"
+__author__ = "Sven Heidemann"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2023 sh-edraft.de"
+__version__ = "2023.4.dev170"
+
+from collections import namedtuple
+
+
 # imports
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro")
+version_info = VersionInfo(major="2023", minor="4", micro="dev170")
