ed2377c2882671a3dc78a14522bcd5b971a04d17
Sven Heidemann <sven.heidemann@sh-edraft.de>
2023-06-13 16:22:54 +0200

    Updated discord.py

--- src/cpl_discord/application/__init__.py
+++ src/cpl_discord/application/__init__.py
@@ -15,7 +15,7 @@ __title__ = "cpl_discord.application"
 __author__ = "Sven Heidemann"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2022 - 2023 sh-edraft.de"
-__version__ = "2023.4.0.post2"
+__version__ = "2023.4.0.post3"
 
 from collections import namedtuple
 
@@ -24,4 +24,4 @@ from collections import namedtuple
 from .discord_bot_application_abc import DiscordBotApplicationABC
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro")
-version_info = VersionInfo(major="2023", minor="4", micro="0.post2")
+version_info = VersionInfo(major="2023", minor="4", micro="0.post3")
--- src/cpl_discord/command/__init__.py
+++ src/cpl_discord/command/__init__.py
@@ -15,7 +15,7 @@ __title__ = "cpl_discord.command"
 __author__ = "Sven Heidemann"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2022 - 2023 sh-edraft.de"
-__version__ = "2023.4.0.post2"
+__version__ = "2023.4.0.post3"
 
 from collections import namedtuple
 
@@ -25,4 +25,4 @@ from .discord_command_abc import DiscordCommandABC
 from .discord_commands_meta import DiscordCogMeta
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro")
-version_info = VersionInfo(major="2023", minor="4", micro="0.post2")
+version_info = VersionInfo(major="2023", minor="4", micro="0.post3")
